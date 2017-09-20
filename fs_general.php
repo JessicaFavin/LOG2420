34bc6a48a008afb21b0e2870bab9e1c72df84727
Jessica Favin <jefav@l4714-09.info.polymtl.ca>
2017-09-20 15:44:10 -0400

    add CSS and html comments

--- fs_general.php
+++ fs_general.php
@@ -4,13 +4,13 @@
 <title>FactSage.com - Introduction to FactSage</title>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
 <STYLE>
-#body A:link {
+A:link {
 	FONT-WEIGHT: bold; COLOR: #0066cc; TEXT-DECORATION: none
 }
-#body A:visited {
+A:visited {
 	COLOR: #660000; FONT-WEIGHT:bold; TEXT-DECORATION: none
 }
-#body A:hover {
+A:hover {
 	COLOR: #000066; BACKGROUND-COLOR: lightgrey; TEXT-DECORATION: none
 }
 .link {
@@ -19,7 +19,7 @@
 </STYLE>
 </head>
 
-<body bgcolor="#666666" id="body">
+<body bgcolor="#666666">
 <table width="95%" border="0" align="center" cellpadding="1" cellspacing="0" bgcolor="#000066">
   <tr>
     <td><table width="100%" border="0" align="center" cellpadding="15" cellspacing="0" bgcolor="#999999">
