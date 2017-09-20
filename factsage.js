f4eb035f7e6d469661eaab7b4e936ec738ae4571
fbeiger <fbeiger971@gmail.com>
2017-09-20 15:08:20 -0400

    Add comments to js

--- factsage.js
+++ factsage.js
@@ -1,23 +1,33 @@
+// jQuery function
+// Will wait for document to be ready before performing updates
 $(document).ready(function(){
 
 	var xhttp = new XMLHttpRequest();
+
+	// Callback executed when a request is sent
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
-		   // Typical action to be performed when the document is ready:
+		   // Action to be performed when the state is ready
 		   document.getElementById("web-page-content").innerHTML = xhttp.responseText;
 			 console.log("displayed");
-		}else {
-			console.log("err");
-			console.log(this.readyState);
-			console.log(this.status);
+		} else {
+			// Fallback
+			console.log("Error");
+			console.log("Ready state: " + this.readyState);
+			console.log("Status: " + this.status);
 		}
 	};
 
+	// To call when a click is fired from our html
+	// Take the filename as argument and send a GET request
 	function loadWebPage(filename) {
 		xhttp.open("GET", filename, true);
 		xhttp.send();
 	}
-	
+
+	// Listen for click events in our html
+	// Assign loadWebPage callback to it with proper filename
 	document.getElementById("general").addEventListener("click", () => loadWebPage('fs_general.php'));
   document.getElementById("reaction").addEventListener("click", () => loadWebPage('fs_reaction.php'));
+
 });
